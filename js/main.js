1c717b56f40f47f521f4d93a885a1c3531b57a81
Hadar-Sabag <hadarsabag5@gmail.com>
2025-03-18 16:14:59 +0200

    Limit the ball size and Add a 1 second transition

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ function onBallClick() {
     var elBall = document.querySelector('.ball')
     gBallSize += 50
 
+    if (gBallSize > 400) gBallSize = 100
+
     elBall.style.width = gBallSize + 'px'
     elBall.style.height = gBallSize + 'px'
 
