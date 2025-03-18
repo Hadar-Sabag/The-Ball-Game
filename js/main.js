3360bee0082c7e982f5b4e41bb31b7ad298afd17
Hadar-Sabag <hadarsabag5@gmail.com>
2025-03-18 17:21:12 +0200

    Add third ball

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@
 
 var gBallSize = 100
 
-function onBallClick(maxDiameter) {
-    const elBall = document.querySelector('.first-ball')
+function onBallClick(maxDiameter, selector) {
+    const elBall = document.querySelector(selector)
+    console.log('elBall: ', elBall)
+    console.log('selector: ', selector)
     const randSize = getRandomInt(20, 60)
     const randColor = getRandomColor()
     gBallSize += randSize
@@ -15,4 +17,18 @@ function onBallClick(maxDiameter) {
 
     elBall.innerText = gBallSize
     elBall.style.backgroundColor = randColor
+}
+
+function onThirdBallClick() {
+    const elBall3 = document.querySelector('.third-ball')
+    const elBall2 = document.querySelector('.sec-ball')
+    const elBall1 = document.querySelector('.first-ball')
+
+    var tempText = elBall1.innerText
+    elBall1.innerText = elBall2.innerText
+    elBall2.innerText = tempText
+
+    var tempColor = elBall1.style.backgroundColor
+    elBall1.style.backgroundColor = elBall2.style.backgroundColor
+    elBall2.style.backgroundColor = tempColor
 }
