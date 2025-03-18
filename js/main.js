4a03eb55b6c3bc14217e973d89f7f08dfbb4eb4b
Hadar-Sabag <hadarsabag5@gmail.com>
2025-03-18 17:53:48 +0200

    Add fourth ball

--- js/main.js
+++ js/main.js
@@ -4,10 +4,9 @@ var gBallSize = 100
 
 function onBallClick(maxDiameter, selector) {
     const elBall = document.querySelector(selector)
-    console.log('elBall: ', elBall)
-    console.log('selector: ', selector)
     const randSize = getRandomInt(20, 60)
     const randColor = getRandomColor()
+
     gBallSize += randSize
 
     if (gBallSize > maxDiameter) gBallSize = 100
@@ -20,7 +19,7 @@ function onBallClick(maxDiameter, selector) {
 }
 
 function onThirdBallClick() {
-    const elBall3 = document.querySelector('.third-ball')
+    // const elBall3 = document.querySelector('.third-ball')
     const elBall2 = document.querySelector('.sec-ball')
     const elBall1 = document.querySelector('.first-ball')
 
@@ -31,4 +30,27 @@ function onThirdBallClick() {
     var tempColor = elBall1.style.backgroundColor
     elBall1.style.backgroundColor = elBall2.style.backgroundColor
     elBall2.style.backgroundColor = tempColor
+}
+
+function onFourthBallClick(maxDiameter1, maxDiameter2) {
+    const elBall2 = document.querySelector('.sec-ball')
+    const elBall1 = document.querySelector('.first-ball')
+    const randSize = getRandomInt(20, 60)
+
+    if (gBallSize > maxDiameter1) gBallSize = maxDiameter1
+    if (gBallSize > maxDiameter2) gBallSize = maxDiameter2
+
+    if (maxDiameter1 < 100) maxDiameter1 = 100
+    if (maxDiameter2 < 100) maxDiameter2 = 100
+
+    maxDiameter1 -= randSize
+    maxDiameter2 -= randSize
+
+    elBall1.style.width = maxDiameter1 + 'px'
+    elBall1.style.height = maxDiameter1 + 'px'
+    elBall1.innerText = maxDiameter1
+
+    elBall2.style.width = maxDiameter2 + 'px'
+    elBall2.style.height = maxDiameter2 + 'px'
+    elBall2.innerText = maxDiameter2
 }
