a0a5cda05c7bd9d8c68af5ab08119f8c79d317c4
Hadar-Sabag <hadarsabag5@gmail.com>
2025-03-18 17:02:33 +0200

    Add a maxDiameter parameter to onBallClick()

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@
 
 var gBallSize = 100
 
-function onBallClick() {
+function onBallClick(maxDiameter) {
     const elBall = document.querySelector('.first-ball')
     const randSize = getRandomInt(20, 60)
     const randColor = getRandomColor()
     gBallSize += randSize
 
-    if (gBallSize > 400) gBallSize = 100
+    if (gBallSize > maxDiameter) gBallSize = 100
 
     elBall.style.width = gBallSize + 'px'
     elBall.style.height = gBallSize + 'px'
