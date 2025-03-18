0cc5db0217eb87e0f5027a468013e6f548f913ed
Hadar-Sabag <hadarsabag5@gmail.com>
2025-03-18 18:34:10 +0200

    Add a timer

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 
 var gBallSize = 100
 var gMoves = 0
+var gTimer = 0
+var timerInterval
 
 function onBallClick(maxDiameter, selector) {
     const elBall = document.querySelector(selector)
@@ -20,6 +22,10 @@ function onBallClick(maxDiameter, selector) {
 
     gMoves++
     setcountMoves()
+
+    if (!timerInterval) {
+        startTimer()
+    }
 }
 
 function onThirdBallClick() {
@@ -37,6 +43,10 @@ function onThirdBallClick() {
 
     gMoves++
     setcountMoves()
+
+    if (!timerInterval) {
+        startTimer()
+    }
 }
 
 function onFourthBallClick(maxDiameter1, maxDiameter2) {
@@ -63,6 +73,10 @@ function onFourthBallClick(maxDiameter1, maxDiameter2) {
 
     gMoves++
     setcountMoves()
+
+    if (!timerInterval) {
+        startTimer()
+    }
 }
 
 function onFifthBallClick() {
@@ -73,6 +87,10 @@ function onFifthBallClick() {
 
     gMoves++
     setcountMoves()
+
+    if (!timerInterval) {
+        startTimer()
+    }
 }
 
 function onSixthBallClick() {
@@ -92,11 +110,25 @@ function onSixthBallClick() {
     document.querySelector('body').style.backgroundColor = 'black'
     document.querySelector('.count').innerText = 0
 
-
+    stopTimer()
 }
 
 function setcountMoves() {
     var elcount = document.querySelector('.count')
     elcount.innerText = gMoves
+}
+
+function startTimer() {
+    timerInterval = setInterval(function () {
+        gTimer++
+        var formattedTime = String(gTimer).padStart(3, '0')
+
+        document.querySelector('.timer').innerText = formattedTime
+    }, 1000)
+}
+
+function stopTimer() {
+    clearInterval(timerInterval)
+    timerInterval = null
 }
 
